5217071cdf2087de5d69c2fcf74d0a854ef6eb2b
Shashank Singh <shashanksi2009@gmail.com>
2017-12-22 20:30:31 +0530

    Create main.js

---
+++ gavel/static/js/main.js
@@ -0,0 +1,11 @@
+$("[data-toggle=popover]").each(function() {
+
+$(this).popover({
+  html: true,
+  content: function() {
+    var id = $(this).attr('id')
+    return $('#popover-content-' + id).html();
+  }
+});
+
+});
