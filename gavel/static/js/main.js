6820350b89e632072ff4bc2fb1a0c20c89d228a9
Shashank Singh <shashanksi2009@gmail.com>
2017-12-22 21:04:57 +0530

    Update main.js

--- gavel/static/js/main.js
+++ gavel/static/js/main.js
@@ -11,7 +11,7 @@ $(this).popover({
 });
 
 $('body').on('click', function (e) {
-    $('[data-toggle="popover"]').each(function () {
+    $('.offFocus').each(function () {
         if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
             $(this).popover('hide');
         }
